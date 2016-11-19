fa6d9570889b3abdb600eb1bca938381ae2a746b
Justin Tsang <justin.b.tsang@gmail.com>
2016-11-19 05:19:13 +0000

    added user basic user authentication code. Data flow is not yet linked

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
+
+
   def create
+      user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
   end
 
   def new
@@ -10,6 +19,12 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+has_secure_password
+
+
+
+
+def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.downcase.strip).try(:authenticate, password)
+    return user
+
+  end
 end
