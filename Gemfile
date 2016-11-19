3cf3e84db387acafca3fae0204c91ebf183d8849
Justin Tsang <justin.b.tsang@gmail.com>
2016-11-19 22:29:14 +0000

    Added navigation links for all pages. Also added a patch route for editing profiles. Required pry gem.

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'actioncable', git: 'git@github.com:rails/actioncable', branch: 'archive'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry'
 end
 
 group :development do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user = User.authenticate_with_credentials(params[:email], params[:password])      # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
-      redirect_to user_path
+      redirect_to user_path(user)
     else
     # If user's login doesn't work, send them back to the login form.
       redirect_to new_session_path
