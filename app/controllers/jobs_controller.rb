1ab26ad57f6eb4ad00f56deb8b47b8fa8917138e
Noam <noam.ascher@gmail.com>
2016-11-20 00:15:03 +0000

    new jobs page rendering

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,20 @@
 class JobsController < ApplicationController
+  def create
+    job = Job.new(job_params)
+    if job.save
+      session[:job_id] = job.id
+      redirect_to job_path
+    else
+      redirect_to new_job_path
+    end
+  end
+
   def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
   end
 
   def edit
