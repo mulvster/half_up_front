167136c4bf152ba257e12a2c628c4b4b33040c47
Noam <noam.ascher@gmail.com>
2016-11-20 00:57:02 +0000

    got video working (very basic, buggy)

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,8 +2,8 @@ class JobsController < ApplicationController
   def create
     job = Job.new(job_params)
     if job.save
-      session[:job_id] = job.id
-      redirect_to job_path
+      #session[:job_id] = job.id
+      redirect_to job_path(job)
     else
       redirect_to new_job_path
     end
@@ -21,6 +21,12 @@ class JobsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:contract_text, :created_at) #more later
   end
 
 end
