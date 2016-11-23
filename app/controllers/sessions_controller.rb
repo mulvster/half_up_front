cd26e54f4bc3f1d98b18880ab1f52fa96b5e71c9
Kyle <Kyleflemington@gmail.com>
2016-11-23 00:57:58 +0000

    Pair Programming - April & Kyle -- Added button and debug console notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,11 @@ class SessionsController < ApplicationController
 
 
   def create
-    uid = request.env['omniauth.auth'].uid
+    auth_hash = request.env['omniauth.auth']
+    puts 'session created'
+    puts 'auth hash'
+    puts auth_hash.inspect
+    uid = auth_hash.uid
     user = User.where(uid: uid).first_or_create
     session[:user_id] = user.id
     redirect_to :root
