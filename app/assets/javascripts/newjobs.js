cbefef5afe056a36a13fdd632972ec4834657ffd
April Mulvey <april.mulvey@gmail.com>
2016-11-24 02:01:41 +0000

    Pair programming April & Kyle have check box js/html logic working about to add columns to the database and add informtion from created checkboxes

--- app/assets/javascripts/newjobs.js
+++ app/assets/javascripts/newjobs.js
@@ -1,40 +1,52 @@
-$(function(){
-	if ($('.new-job').length===0){
-		return;
-	} 	
+$(function () {
+    if ($('.new-job').length === 0) {
+        return;
+    }
 
-	$('label.radio-btn input[type="radio"]').on('click', function(e){
-		console.log("label click")
-		$(this).closest('section').find('label.radio-btn').removeClass('active');
-		$(this).parents('label.radio-btn').addClass('active');
-	});
+    $('label.radio-btn input[type="radio"]').on('click', function (e) {
+        console.log("label click")
+        $(this).closest('section').find('label.radio-btn').removeClass('active');
+        $(this).parents('label.radio-btn').addClass('active');
+    });
+
+    $('.check-btn').on("click", function () {
+        // grouping all the checkbox using the classname
+        // var checkboxes = $(".check-btn input:checked");
+        var checkbox = $(this).find('input');
+        if (checkbox.is(':checked')) {
+            checkbox.removeProp('checked');
+        }
+        else {
+            checkbox.prop('checked', 'checked');
+        }
+    });
+
 });
-
 
 
 /* 
  $(function()
-{
-    $(document).on('click', '.btn-add', function(e)
-    {
-        e.preventDefault();
+ {
+ $(document).on('click', '.btn-add', function(e)
+ {
+ e.preventDefault();
 
-        var controlForm = $('.controls form:first'),
-            currentEntry = $(this).parents('.entry:first'),
-            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+ var controlForm = $('.controls form:first'),
+ currentEntry = $(this).parents('.entry:first'),
+ newEntry = $(currentEntry.clone()).appendTo(controlForm);
 
-        newEntry.find('input').val('');
-        controlForm.find('.entry:not(:last) .btn-add')
-            .removeClass('btn-add').addClass('btn-remove')
-            .removeClass('btn-success').addClass('btn-danger')
-            .html('<span class="glyphicon glyphicon-minus"></span>');
-    }).on('click', '.btn-remove', function(e)
-    {
-        $(this).parents('.entry:first').remove();
+ newEntry.find('input').val('');
+ controlForm.find('.entry:not(:last) .btn-add')
+ .removeClass('btn-add').addClass('btn-remove')
+ .removeClass('btn-success').addClass('btn-danger')
+ .html('<span class="glyphicon glyphicon-minus"></span>');
+ }).on('click', '.btn-remove', function(e)
+ {
+ $(this).parents('.entry:first').remove();
 
-        e.preventDefault();
-        return false;
-    });
-});
+ e.preventDefault();
+ return false;
+ });
+ });
 
-*/
+ */
