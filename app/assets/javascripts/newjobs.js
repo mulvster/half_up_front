cca6152514fe52ace539c678986c4965fa3d223c
Kyle <Kyleflemington@gmail.com>
2016-11-24 21:56:11 +0000

    Pair Programming: April & Kyle -- have input form dynamically being added to the page to add website links for new/jobs page

--- app/assets/javascripts/newjobs.js
+++ app/assets/javascripts/newjobs.js
@@ -21,47 +21,11 @@ $(function () {
         $(this).toggleClass('hovered');
     });
 
-
-    var i = $('input').size() + 1;
-
         $('#add').click(function() {
-        $('<div><input type="text" class="field" name="dynamic[]" value="' + i + '" /></div>').fadeIn('slow').appendTo('.inputs'); i++;
+        $('<div><input type="text" class="field" name="dynamic[]" value="'+ '" /></div>').fadeIn('fast').appendTo('.inputs');
         });
 
         $('#remove').click(function() {
-        if(i > 1) {
         $('.field:last').remove();
-        i--;
-        }
         });
-        $('#reset').click(function() {
-        while(i > 2) {
-        $('.field:last').remove();
-        i--;
-        }
-    });
-
-    // here's our click function for when the forms submitted
-
-    $('.submit').click(function(){
-
-        var answers = [];
-
-        $.each($('.field'), function() {
-
-        answers.push($(this).val());
-
-        });
-
-        if(answers.length == 0) {
-
-        answers = "none";
-
-    }
-
-    alert(answers);
-
-    return false;
-
-    });
 });
