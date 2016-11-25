de95b076fe9f237a6bbf9ecf73f115b47bd6f068
April Mulvey <april.mulvey@gmail.com>
2016-11-25 04:56:38 +0000

    started styling and implementation of buttons on user/id page. Pair Programming April & Kyle

---
+++ app/assets/javascripts/userprofile.js
@@ -0,0 +1,11 @@
+var activeEl = 0;
+$(function() {
+    var items = $('.btn-nav');
+    $( items[activeEl] ).addClass('active');
+    $( ".btn-nav" ).click(function() {
+        $( items[activeEl] ).removeClass('active');
+        $( this ).addClass('active');
+        activeEl = $( ".btn-nav" ).index( this );
+
+    });
+});
