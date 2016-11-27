6206fdffcfcb62a2b365460be298e7452d990434
Noam <noam.ascher@gmail.com>
2016-11-27 04:21:44 +0000

    pre-empting merge conflicts through hand-merging feature/milestones4: routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :new, :edit, :show, :update]
 
-  resources :jobs, only: [:create, :new, :show, :edit, :destroy, :index] do
+  resources :jobs, only: [:create, :new, :edit, :show, :update, :destroy, :index] do
     resources :milestones, only: [:create, :edit, :destroy, :update] do
       resources :requirements, only: [:create, :edit, :destroy, :update]
     end
