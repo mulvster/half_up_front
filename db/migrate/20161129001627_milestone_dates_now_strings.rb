b560380aac2ca1607cb39967853a8c1641581679
Noam <noam.ascher@gmail.com>
2017-01-10 03:44:24 +0000

    trying out production db (pt 4 - slight migration touch-up)

--- db/migrate/20161129001627_milestone_dates_now_strings.rb
+++ db/migrate/20161129001627_milestone_dates_now_strings.rb
@@ -2,7 +2,7 @@ class MilestoneDatesNowStrings < ActiveRecord::Migration
   def change
     change_column :milestones, :start_date, :string
     change_column :milestones, :end_date, :string
-    change_column :milestones, :created_at, :datetime
-    change_column :milestones, :updated_at, :datetime
+    #change_column :milestones, :created_at, :datetime
+    #change_column :milestones, :updated_at, :datetime
   end
 end
