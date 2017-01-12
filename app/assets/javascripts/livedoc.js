407f6051822f85551aab0761a95cc41bc7eb6440
Noam <noam.ascher@gmail.com>
2017-01-12 05:42:31 +0000

    websocket fix attempt 3

--- app/assets/javascripts/livedoc.js
+++ app/assets/javascripts/livedoc.js
@@ -1,6 +1,6 @@
 var MILESTONE_DATA_ATTRIBUTE_NAME = 'data-milestone-id';
 
-var dispatcher = new WebSocketRails('138.197.134.47:80/websocket', false);
+var dispatcher = new WebSocketRails('127.0.0.1:6001/websocket', false);
 
 function handleUpdate(event) {
   dispatcher.trigger('update', {
