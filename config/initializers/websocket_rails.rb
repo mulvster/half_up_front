6b2075ac4bfc384329aceba57a17aa198908d3b3
Noam <noam.ascher@gmail.com>
2017-01-17 22:12:38 +0000

    post sites-enabled bug-fix websockets fix attempt: 12

--- config/initializers/websocket_rails.rb
+++ config/initializers/websocket_rails.rb
@@ -16,7 +16,7 @@ WebsocketRails.setup do |config|
   # Start the standalone server with rake websocket_rails:start_server
   # * Requires Redis
   config.standalone = true
-  config.redis_options = {:host => 'your.host', :port => '6379'}
+  config.redis_options = {:host => window.location.hostname, :port => '6379'}
   # Change to true to enable channel synchronization between
   # multiple server instances.
   # * Requires Redis.
